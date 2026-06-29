034a6f75accacbb2d95e11e058500b8c6f518888
Christian <zen.chishiki404@gmail.com>
2026-06-29 13:07:22 +0800

    Run migrate on WSGI startup as fallback for Render

--- ict_inventory/wsgi.py
+++ ict_inventory/wsgi.py
@@ -8,9 +8,18 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ict_inventory.settings')
+
+try:
+    import django
+    django.setup()
+    call_command('migrate', '--run-syncdb', verbosity=1)
+except Exception as e:
+    print(f"Migration error: {e}", file=sys.stderr)
 
 application = get_wsgi_application()
