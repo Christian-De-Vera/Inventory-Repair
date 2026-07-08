b4dfc05388a48d96bbfe22d4de8c91f8af7f2290
Christian <zen.chishiki404@gmail.com>
2026-07-08 14:28:16 +0800

    Fix STATIC_ROOT to match nginx /static/ root so admin CSS loads

--- ict_inventory/settings.py
+++ ict_inventory/settings.py
@@ -151,7 +151,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # WhiteNoise configuration for Django 6.x
 STORAGES = {
